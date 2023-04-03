aa244db327b737ad161184c4c1362c8dae25f79d
Azeem <perfectdesigns001@gmail.com>
2023-04-03 19:37:13 +0500

    fetched data

--- script.js
+++ script.js
@@ -5,12 +5,31 @@ const tableDate = document.querySelector(".table-data")
 
 // VARIABLES DECLARATION -----------------------
 const url = "https://laravel-world.com/api/countries?"
+const url1 = "https://laravel-world.com/api/currencies?"
 // EVENT LISTNERS -----------------------------
 
 // PROCEDURES ---------------------------------
 
 async function getCountryCurrency(){
-    const response = await fetch(url)
-    const data = await response.json()
-    console
+    const resCountry = await fetch(url)
+    const dataCountry = await resCountry.json()
+    const resCurrency = await fetch(url1)
+    const dataCurrency = await resCurrency.json()
+    console.log(dataCountry.data.length)
+    tableDate.innerHTML = `
+    <tr>
+        <th>Country</th>
+        <th>Currency</th>
+    </tr>
+    `
+    
+    for(i=0; i<dataCountry.data.length; i++){
+        tableDate.innerHTML += `
+        <tr>
+            <td>${dataCountry.data[i].name}</td>
+            <td>${dataCurrency.data[i].name}</td>
+        </tr>
+        `
+    }
 }
+getCountryCurrency()
